b56e327d56b7528d52bba5f8c0a62cf0933c875c
cadehaley <cade.haley@colorado.edu>
2016-11-14 19:02:10 -0700

    Added ability to pull content of a note from the db using note_id. See comments for how to do this

--- db.sql
+++ db.sql
@@ -1,5 +1,5 @@
--- CREATE DATABASE Timeless;
 /*
+CREATE DATABASE Timeless;
 CREATE TABLE `Timeless`.`tbl_user` (
   `user_uid` BIGINT UNIQUE AUTO_INCREMENT,
   `user_email` VARCHAR(45) NULL,
@@ -65,3 +65,71 @@ END$$
 DELIMITER ;
 
 -- End 'create user' procedure
+
+-- 'spGetNote' procedure
+
+
+DROP procedure IF EXISTS `spGetNote`;
+
+DELIMITER $$
+USE `Timeless`$$
+CREATE PROCEDURE `spGetNote` (
+IN p_note_id varchar(45)
+)
+BEGIN
+
+if ( SELECT EXISTS (select 1 FROM tbl_notes WHERE note_nid = p_note_id) ) THEN
+    
+    select * FROM tbl_notes WHERE note_nid = p_note_id;
+
+ELSE
+
+    select 'Mysql error: Note corresponding to note_id not found';
+
+END IF;
+
+END$$
+
+DELIMITER ;
+
+-- End 'spGetNote' procedure
+
+
+
+-- TODO: Make a 'save note' procedure
+
+USE `Timeless`;
+
+DROP procedure IF EXISTS `spSaveNote`;
+
+DELIMITER $$
+USE `Timeless`$$
+CREATE PROCEDURE `spSaveNote` (
+IN p_note_id varchar(45),
+IN p_note_body varchar(2048)
+)
+BEGIN
+
+if ( select exists (select 1 from tbl_notes where note_name = p_note_name) ) THEN
+    UPDATE tbl_notes
+    SET note_content=p_note_body
+    WHERE note_id=p_note_id;
+
+ELSE
+
+insert into tbl_notes
+(
+    note_content
+)
+values
+(
+    p_note_body
+);
+
+END IF;
+
+END$$
+
+DELIMITER ;
+
+-- End 'save note' procedure
